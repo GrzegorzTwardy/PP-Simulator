1f335d8d9bccb0af370fdf596386c1f0afd6f390
GrzegorzTwardy <grzes.twardy@gmail.com>
2024-10-26 13:21:19 +0200

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,33 @@
+﻿namespace Simulator;
+
+public class Creature
+{
+    private string name;
+    private int level;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Level
+    {
+        get { return level; }
+        set { level = value > 0 ? value : 1; }
+    }
+    public string Info => $"{Name} [{Level}]";
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() {}
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -5,5 +5,12 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting simulator!\n");
+
+        Creature c = new Creature("Shrek", 7);
+        c.SayHi();
+        Console.WriteLine(c.Info);
+
+        Animals a = new() { Description = "Dogs" };
+        Console.WriteLine(a.Info);
     }
 }

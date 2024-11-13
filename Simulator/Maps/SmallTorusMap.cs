12c8bf6f8caae3115d377e5a50ca61ab976981ec
s-A301-08 <s-A301-08@K-A301-08>
2024-11-13 16:19:21 +0100

    pracownia-13-11

---
+++ Simulator/Maps/SmallTorusMap.cs
@@ -0,0 +1,26 @@
+﻿namespace Simulator.Maps;
+
+public class SmallTorusMap : Map
+{
+    public int Size { get; }
+
+    public SmallTorusMap(int size)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Exist(Point p)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Point Next(Point p, Direction d)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        throw new NotImplementedException();
+    }
+}
